e68c705a21e54e97b4ce87f1f045f278408c90d6
hxd <houxiaodong_5@163.com>
2019-08-23 11:32:47 +0800

    修复未关闭jedis连接可能出现问题的bug

--- src/main/java/com/hxd/test1/controller/RedisController.java
+++ src/main/java/com/hxd/test1/controller/RedisController.java
@@ -29,21 +29,30 @@ public class RedisController {
      * */
     @GetMapping()
     public ResponseEntity<List<Object>> queryAll(){
-        Jedis jedis = slaveJedisPool.getResource();
-        System.out.println(slaveJedisPool);
-        List<Object> list = new ArrayList<>();
-        for(int a=0;a<5;a++){
-            byte[] key = jedis.randomBinaryKey();
-            System.out.println(key);
-            if (key!=null)
-            list.add(ByteArrayUtils.bytesToObject(jedis.get(key)));
+        Jedis jedis=null;
+        try {
+            jedis = slaveJedisPool.getResource();
+            System.out.println(slaveJedisPool);
+            List<Object> list = new ArrayList<>();
+            for(int a=0;a<5;a++){
+                byte[] key = jedis.randomBinaryKey();
+                System.out.println(key);
+                if (key!=null)
+                list.add(ByteArrayUtils.bytesToObject(jedis.get(key)));
 
+            }
+            System.out.println(jedis.get("hxd"));
+            list.add(jedis.get("hxd"));
+
+            jedis.close();
+            return ResponseEntity.ok(list);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        } finally {
+            if(jedis!=null){
+                jedis.close();
+            }
         }
-        System.out.println(jedis.get("hxd"));
-        list.add(jedis.get("hxd"));
-
-        jedis.close();
-       return ResponseEntity.ok(list);
 
     }
 
@@ -52,19 +61,27 @@ public class RedisController {
      * */
     @GetMapping("/1")
     public ResponseEntity<List<Object>> query(){
-        Jedis jedis = masterJedisPool.getResource();
-        List<Object> list = new ArrayList<>();
-        //之前数据使用byte[] 存储，不好获取byte[]类型key,只好通过随机去拿
-        for(int a=0;a<5;a++){
-            byte[] key = jedis.randomBinaryKey();
-            System.out.println(key);
-            if (key!=null)
-                list.add(ByteArrayUtils.bytesToObject(jedis.get(key)));
+        Jedis jedis=null;
+        try {
+            jedis = masterJedisPool.getResource();
+            List<Object> list = new ArrayList<>();
+            //之前数据使用byte[] 存储，不好获取byte[]类型key,只好通过随机去拿
+            for(int a=0;a<5;a++){
+                byte[] key = jedis.randomBinaryKey();
+                System.out.println(key);
+                if (key!=null)
+                    list.add(ByteArrayUtils.bytesToObject(jedis.get(key)));
 
+            }
+            list.add(jedis.get("hxd"));
+            return ResponseEntity.ok(list);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        } finally {
+            if(jedis!=null){
+                jedis.close();
+            }
         }
-        list.add(jedis.get("hxd"));
-        jedis.close();
-        return ResponseEntity.ok(list);
     }
 
     /**
@@ -72,14 +89,27 @@ public class RedisController {
      * */
     @PostMapping("/test")
     public ResponseEntity<User> insert(@RequestBody User user){
-        Jedis master = masterJedisPool.getResource();
-        master.set("test", YjsonUtil.toJson(user));
-        Jedis slave = slaveJedisPool.getResource();
-        String data = slave.get("test");
-        User fromJson = YjsonUtil.fromJson(data, User.class);
-        System.out.println(fromJson);
-        master.close();
-        slave.close();
-        return ResponseEntity.ok(fromJson);
+        Jedis master = null;
+        Jedis slave = null;
+        User fromJson = null;
+        try {
+            master = masterJedisPool.getResource();
+            master.set("test", YjsonUtil.toJson(user));
+            slave = slaveJedisPool.getResource();
+            String data = slave.get("test");
+            fromJson = YjsonUtil.fromJson(data, User.class);
+            System.out.println(fromJson);
+            return ResponseEntity.ok(fromJson);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }finally {
+            if(master!=null){
+                master.close();
+            }
+            if(slave!=null){
+                slave.close();
+            }
+        }
+
     }
 }
